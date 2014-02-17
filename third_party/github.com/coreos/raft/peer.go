a91ef9c3b271d1983312ba9dc8bb920608ed2355
Yicheng Qin <qycqycqycqycqyc@gmail.com>
2014-02-16 17:24:20 -0800

    Beg no say

--- third_party/github.com/coreos/raft/peer.go
+++ third_party/github.com/coreos/raft/peer.go
@@ -79,7 +79,7 @@ func (p *Peer) setPrevLogIndex(value uint64) {
 
 // Starts the peer heartbeat.
 func (p *Peer) startHeartbeat() {
-	p.stopChan = make(chan bool)
+	p.stopChan = make(chan bool, 1)
 	c := make(chan bool)
 	go p.heartbeat(c)
 	<-c
@@ -87,7 +87,13 @@ func (p *Peer) startHeartbeat() {
 
 // Stops the peer heartbeat.
 func (p *Peer) stopHeartbeat(flush bool) {
-	p.stopChan <- flush
+	// non-blocking send to avoid deadlock
+	// buffer size of 1 ensures that we can always receive the 1st one
+	// And only the 1st one matters
+	select {
+	case p.stopChan <- flush:
+	default:
+	}
 }
 
 //--------------------------------------
